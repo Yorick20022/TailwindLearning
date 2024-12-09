e903fb8342bdaf6a825e8b0449fc056efabcbe83
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2024-12-09 23:19:37 +0100

    added more to header

--- youtube/src/App.tsx
+++ youtube/src/App.tsx
@@ -4,15 +4,41 @@ function App() {
 
 
   return (
-    <>
-      {/* Header div */}
-      <div>
-        <div className='p-5 ml-1'>
+    <div className="h-dvh w-full relative">
+      {/* <!--Header--> */}
+
+      {/* Left part */}
+      <div className="h-14 w-full relative bg-white flex items-center px-6 justify-between">
+        <div className='flex items-center gap-x-6 h-full relative'>
           <img src='../assets/sidebarbutton.svg'></img>
-          <img className='w-52 px-14 -mt-6' src='../assets/youtube-6.svg'></img>
+          <img className='w-[93px]' src='../assets/youtube-6.svg'></img>
+        </div>
+
+        {/* Center part */}
+        <div>
+          <input type='text' placeholder='Search' className='w-[35rem] border-yellow-400 rounded-xl leading-8'></input>
+        </div>
+
+        {/* Right part */}
+        <div>
+          <p>hi</p>
         </div>
       </div>
-    </>
+
+
+
+      {/* <!--LowerContainer--> */}
+      <div className="h-[calc(100vh-4rem)] bg-neutral-200 flex">
+        {/* <!--Sidebar--> */}
+        <div className="w-56 bg-neutral-400 h-full">
+
+        </div>
+        {/* <!--Main Content--> */}
+        <div className="grow bg-neutral-500 h-full">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
