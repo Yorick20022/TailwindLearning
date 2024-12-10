ffbc40f8a3d0815f17db28d6161be757dbe78eb5
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2024-12-10 18:46:30 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA :333333

--- youtube/src/App.tsx
+++ youtube/src/App.tsx
@@ -10,32 +10,37 @@ function App() {
       {/* Left part */}
       <div className="h-14 w-full relative bg-white flex items-center px-6 justify-between">
         <div className='flex items-center gap-x-6 h-full relative'>
-          <img src='../assets/sidebarbutton.svg'></img>
+          <img className='hover:cursor-pointer' src='../assets/sidebarbutton.svg'></img>
           <img className='w-[93px]' src='../assets/youtube-6.svg'></img>
         </div>
 
         {/* Center part */}
-        <div>
-          <input type='text' placeholder='Search' className='w-[35rem] border-yellow-400 rounded-xl leading-8'></input>
+        <div className=''>
+          {/* <input type='text' placeholder='Search' className='border-2 border-solid outline-none'></input> */}
         </div>
 
         {/* Right part */}
-        <div>
-          <p>hi</p>
+        <div className='flex relative gap-x-6 px-2'>
+          <img className='hover:cursor-pointer' src='../assets/create.svg'></img>
+          <img className='hover:cursor-pointer' src='../assets/notifications.svg'></img>
+          <img className="h-8 w-8 rounded-3xl hover:cursor-pointer" src='../assets/pf.jpg'></img>
         </div>
       </div>
 
 
 
       {/* <!--LowerContainer--> */}
-      <div className="h-[calc(100vh-4rem)] bg-neutral-200 flex">
+      <div className="h-[calc(100vh-4rem)] flex">
+
         {/* <!--Sidebar--> */}
-        <div className="w-56 bg-neutral-400 h-full">
-
+        <div className="w-56 h-full p-6 flex">
+          <div className='flex items-center flex-col'>
+            
+          </div>
         </div>
-        {/* <!--Main Content--> */}
-        <div className="grow bg-neutral-500 h-full">
 
+        {/* <!--Main Content--> */}
+        <div className="grow  h-full">
         </div>
       </div>
     </div>
