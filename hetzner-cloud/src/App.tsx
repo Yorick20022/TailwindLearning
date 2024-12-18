87f832d2b295d3c0ac43bff28b4aaa5e8acb3a75
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2024-12-18 21:16:22 +0100

    started with top bar hetzner

--- hetzner-cloud/src/App.tsx
+++ hetzner-cloud/src/App.tsx
@@ -3,9 +3,30 @@ import './index.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    // Full page
+    <div className='w-full h-dvh bg-[#f5f5f5]'>
+
+      {/* Top bar */}
+      <div className='flex w-full p-5 px-7 bg-red-500'>
+
+        {/* Hetzner, cloud and gerland div */}
+        <div className='flex'>
+
+          {/* Hetzner and cloud */}
+          <div className='flex h-6 gap-x-2'>
+            <img src='/hetzner-cloud.svg'></img>
+            <span className='text-xl font-semibold text-[#383838]'>Cloud</span>
+          </div>
+
+          {/* Divider */}
+          <div className='flex gap-x-2'>
+            <span className='text-sm text-gray-600'></span>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
